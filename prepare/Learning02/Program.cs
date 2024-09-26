eee459bb2902ea05e0d19e562223a8647e2870bc
jonacardiel <car20039@byui.edu>
2024-09-26 13:48:52 -0600

    homework w2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,57 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Get the grade percentage from the user
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        // Determine the letter grade
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign (optional stretch challenge)
+        string sign = "";
+        if (letter != "A" && letter != "F" && gradePercentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (letter != "F" && gradePercentage % 10 < 3)
+        {
+            sign = "-";
+        }
+
+        // Display the letter grade and sign (optional stretch challenge)
+        Console.WriteLine("Your letter grade is: " + letter + sign);
+
+        // Determine if the user passed the course
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard, you'll get there next time!");
+        }
     }
 }
