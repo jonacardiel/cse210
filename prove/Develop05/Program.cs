b7ecaa85dfd61539e0e518fefadbad37f7d374da
jonacardiel <car20039@byui.edu>
2024-11-14 11:59:32 -0700

    W09 HW

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,80 @@
-using System;
-
-class Program
+public abstract class Shape
 {
-    static void Main(string[] args)
+    private string _color;
+
+    public Shape(string color)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        _color = color;
+    }
+
+    public string Color
+    {
+        get { return _color; }
+        set { _color = value; }
+    }
+
+    public abstract double GetArea();
+}
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double _width;
+    private double _height;
+
+    public Rectangle(string color, double width, double height) : base(color)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    public override double GetArea()
+    {
+        return _width * _height;
+    }
+}
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
